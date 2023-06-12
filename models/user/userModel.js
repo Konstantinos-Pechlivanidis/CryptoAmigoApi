d0c3e65041645a9d5dc6e1710021fb4c49b29d1e
kwstaspexli <kostaspechli2@gmail.com>
2023-06-12 12:29:49 +0300

    Error Handling

--- models/user/userModel.js
+++ models/user/userModel.js
@@ -1,80 +1,70 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt"); //used to hash the passwords
-const validator = require("validator"); //used to make the user write valid emails and passwords to the inputs
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt') //used to hash the passwords
+const validator = require('validator') //used to make the user write valid emails and passwords to the inputs
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
 
 const userSchema = new Schema({
   email: {
     type: String,
     required: true,
-    unique: true, //if a record already exists in the database, mongoose wont let us save it
+    unique: true  //if a record already exists in the database, mongoose wont let us save it
   },
   password: {
     type: String,
-    required: true,
-  },
-});
+    required: true
+  }
+})
 
 // static signup method
 // its not an arrow function because i couldnt use "this" keyword
-userSchema.statics.signup = async function (email, password) {
+userSchema.statics.signup = async function(email, password) {
+
   // validation
-  if (!email || !password) {
-    //checks if i have a value for email or password or both
-    throw Error("All fields must be filled");
+  if (!email || !password) { //checks if i have a value for email or password or both
+    throw Error('All fields must be filled')
   }
   if (!validator.isEmail(email)) {
-    throw Error("Email not valid");
+    throw Error('Email not valid')
   }
   if (!validator.isStrongPassword(password)) {
-    throw Error(
-      <div>
-        <p>Password not strong enough. Contain:</p>
-        <ul>
-          <li>At least 8 characters length</li>
-          <li>At least 1 number (0...9)</li>
-          <li>At least 1 lowercase letter (a...z)</li>
-          <li>At least 1 uppercase letter (A...Z)</li>
-          <li>At least 1 special symbol (!...$)</li>
-        </ul>
-      </div>
-    );
+    throw Error('Password not strong enough')
   }
 
-  const exists = await this.findOne({ email }); //this equals to user
+  const exists = await this.findOne({ email }) //this equals to user
 
   if (exists) {
-    throw Error("Email already in use");
+    throw Error('Email already in use')
   }
 
-  const salt = await bcrypt.genSalt(10); //adds 10 chars to the String-Password before it gets hashed (extra protection)
-  const hash = await bcrypt.hash(password, salt);
+  const salt = await bcrypt.genSalt(10) //adds 10 chars to the String-Password before it gets hashed (extra protection)
+  const hash = await bcrypt.hash(password, salt) 
 
-  const user = await this.create({ email, password: hash });
+  const user = await this.create({ email, password: hash })
 
-  return user;
-};
+  return user
+}
 
 // static login method
-userSchema.statics.login = async function (email, password) {
+userSchema.statics.login = async function(email, password) {
+
   if (!email || !password) {
-    throw Error("All fields must be filled");
+    throw Error('All fields must be filled')
   }
 
-  const user = await this.findOne({ email });
+  const user = await this.findOne({ email })
   if (!user) {
-    throw Error("Incorrect email");
+    throw Error('Incorrect email')
   }
 
-  const match = await bcrypt.compare(password, user.password);
+  const match = await bcrypt.compare(password, user.password)
   // compares the passwords, the first is plain text and the other is hashed password
   // bcrypt can compare them and return if the passwords are the same
   if (!match) {
-    throw Error("Incorrect password");
+    throw Error('Incorrect password')
   }
 
-  return user;
-};
+  return user
+}
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema)
